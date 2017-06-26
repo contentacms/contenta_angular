4ea8490175727b0dc3b8883e24122f3b36e1b3dc
Joao Garin <joao.garin@gmail.com>
2017-06-26 15:50:02 +0100

    fix(card): added basic test

--- src/app/components/card/card.component.spec.ts
+++ src/app/components/card/card.component.spec.ts
@@ -1,25 +1,40 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+import { MdCardModule } from '@angular/material';
 
 import { CardComponent } from './card.component';
 
 describe('CardComponent', () => {
   let component: CardComponent;
   let fixture: ComponentFixture<CardComponent>;
+  let element: DebugElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ CardComponent ]
+      declarations: [ CardComponent ],
+      imports: [
+        MdCardModule,
+      ],
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(CardComponent);
+    element = fixture.debugElement;
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
   it('should be created', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should render title input in the template', () => {
+    component.title = 'Contenta card';
+    fixture.detectChanges();
+    let elm = element.query(By.css('.card-title')).nativeElement;;
+    expect(elm.innerHTML).toBe('Contenta card');
   });
 });
