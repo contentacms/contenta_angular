4769eff197d9cecb2ce8b82557ca416972877db3
Matt Davis <matt.davis@mediacurrent.com>
2017-07-25 17:56:56 -0700

    Making app.browser.module.ts for browser-specific stuff

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,7 +18,6 @@ import {
   MdSidenavModule,
   MdToolbarModule
 } from '@angular/material';
-import 'hammerjs';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { StoreModule } from '@ngrx/store';
