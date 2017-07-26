0a1e65cb138025283f1ac80d2593d32a3819e487
Joao Garin <joao.garin@gmail.com>
2017-07-26 15:09:12 +0100

    remove log

--- src/app/components/recipe-details/recipe-details.component.ts
+++ src/app/components/recipe-details/recipe-details.component.ts
@@ -18,7 +18,6 @@ export class RecipeDetailsComponent {
     store.select('app').subscribe(t => {
       const id = route.snapshot.paramMap.get('id');
       this.recipe = t.recipes[id];
-      console.log(this.recipe);
     });
   }
 }
