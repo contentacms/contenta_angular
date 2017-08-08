80745b263aea207e6f745cecb1f635a68023154f
Matt Davis <matt.davis@mediacurrent.com>
2017-08-08 11:53:31 -0700

    Latest moves towards firebase cloud functions w universal

--- functions/index.js
+++ functions/index.js
@@ -1,21 +1,32 @@
-const functions = require('firebase-functions');
+// Load zone.js for the server.
 require('zone.js/dist/zone-node');
 require('reflect-metadata');
-const fs = require('fs');
-const { AppServerModuleNgFactory } = require('../dist-server/main.bundle');
-const { renderModuleFactory } = require('@angular/platform-server');
+const functions = require('firebase-functions');
+const express = require('express');
+const path = require('path')
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
- exports.app = functions.https.onRequest((req, res) => {
-    renderModuleFactory(AppServerModuleNgFactory, {
-        url: '/',
-        document: fs.readFileSync('dist-client/index.html',  'utf8')
-    }).then(html => {
-        res.send(html);
-    });
- });
+// Import renderModuleFactory from @angular/platform-server.
+const renderModuleFactory = require('@angular/platform-server').renderModuleFactory;
+
+// Import the AOT compiled factory for your AppServerModule.
+// This import will change with the hash of your built server bundle.
+const AppServerModuleNgFactory = require('./dist-server/main.bundle').AppServerModuleNgFactory;
+
+// Load the index.html file.
+const index = require('fs').readFileSync(path.resolve(__dirname, './index.html'), 'utf8');
+
+const app = express();
+
+app.get('/', function(req, res) {
+  renderModuleFactory(AppServerModuleNgFactory, {document: index, url: '/'})
+      .then(function(html) {
+         res.send(html);
+      }).catch( function(e) {
+         console.log(e)
+      });
+});
+
+exports.app = functions.https.onRequest(app);
 
  exports.helloWorld = functions.https.onRequest((request, response) => {
   response.send("Hello from Firebase!");
